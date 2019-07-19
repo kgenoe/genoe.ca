ad7427ba21c53c8fd72cd266ca036155f76eb7a8
Kyle Genoe <kylegenoe@hotmail.com>
2019-07-19 15:53:00 -0400

    Add blog posts on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,52 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-  </Layout>
-)
+export default ({ data }) => {
+  console.log(postsFromData(data))
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postsFromData(data)}
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+{
+  allMarkdownRemark (
+    filter: { fields: { slug: { ne: null} } },
+    sort: {order: ASC, fields: [frontmatter___date]},
+    limit: 2
+  ){
+    edges {
+      node {
+        html
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+}
+`
+
+function postsFromData(data) {
+  const posts= [];
+  data.allMarkdownRemark.edges.forEach(( { node }) => {
+    posts.push(
+    <div>
+      <h2>{node.frontmatter.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: node.html }} />
+      <br/>
+      <hr/>
+      <br/>
+      <br/>
+    </div>
+    )
+  });
+  return posts;
+}
